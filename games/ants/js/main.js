4ebd6483734da709f246dddb17d0b4ff6edad972
Your Name <you@example.com>
2016-12-22 10:17:42 -0600

    debugging git macros

--- games/ants/js/main.js
+++ games/ants/js/main.js
@@ -62,7 +62,7 @@ var mooreNeighborhood = (function mooreNeighborhood$(w = this.w, h = this.h, wei
   return m;
 });
 var matrixCenter = (function matrixCenter$(width, height) {
-  /* matrix-center eval.sibilant:46:0 */
+  /* matrix-center eval.sibilant:44:0 */
 
   return Math.round((((width * height) - 1) / 2));
 });
@@ -476,7 +476,7 @@ const Ant = extend(Entity, {
   _nearNest( nest = this.nest,ant = this.ant,collision = this.collision ){ 
     
       return (function() {
-        /* eval.sibilant:32:8 */
+        /* eval.sibilant:30:8 */
       
         let true__QUERY = false;
         eachInArea(collision, ant, (spot, i, j, x, y) => {
@@ -620,13 +620,13 @@ const Plant = extend(Entity, {
    }
  });
 Map.prototype.each = (function Map$prototype$each$(f) {
-  /* Map.prototype.each eval.sibilant:290:0 */
+  /* Map.prototype.each eval.sibilant:288:0 */
 
   this.forEach(f);
   return this;
 });
 var start = (function start$(sim) {
-  /* start eval.sibilant:292:0 */
+  /* start eval.sibilant:290:0 */
 
   let plants = create(EntityGroup)(Plant);
   let reds = create(Colony)({
