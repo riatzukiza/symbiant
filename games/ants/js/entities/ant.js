ed47fa4c8f3f5d9be466c9675d8b9c0050960df2
Your Name <you@example.com>
2016-12-25 17:44:42 -0600

    compiled /home/aaron/devel/symbiant/games/ants/sibilant/js/entities/ant.sibilant

--- games/ants/js/entities/ant.js
+++ games/ants/js/entities/ant.js
@@ -12,6 +12,9 @@ const {
 const { 
   Pheremones
  } = require("../pheremons");
+const { 
+  Colony
+ } = require("./colony");
 var mooreNeighborhood = (function mooreNeighborhood$(w = this.w, h = this.h, weight = 1, c = 0) {
   /* moore-neighborhood deps.sibilant:61:8 */
 
@@ -24,7 +27,7 @@ var mooreNeighborhood = (function mooreNeighborhood$(w = this.w, h = this.h, wei
   return m;
 });
 var matrixCenter = (function matrixCenter$(width, height) {
-  /* matrix-center eval.sibilant:8:0 */
+  /* matrix-center eval.sibilant:9:0 */
 
   return Math.round((((width * height) - 1) / 2));
 });
