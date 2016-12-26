a683cd015841b47a279390f084cdff820e0737a0
Your Name <you@example.com>
2016-12-26 14:23:25 -0600

    compiled /home/aaron/devel/symbiant/games/ants/sibilant/js/main.sibilant

--- games/ants/js/main.js
+++ games/ants/js/main.js
@@ -134,7 +134,7 @@ var start = (function start$(sim) {
   return sim.start().on("tick", (now, ticks) => {
   	
     (function() {
-      if ((ticks % 1) === 0) {
+      if ((ticks % 10) === 0) {
         return plants.update();
       }
     }).call(this);
