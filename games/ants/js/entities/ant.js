e8f2e78d5a1daaff25688fa1fd0b5c225f702af8
Your Name <you@example.com>
2016-12-28 12:08:43 -0600

    compiled /home/aaron/devel/symbiant/games/ants/sibilant/js/entities/ant.sibilant

--- games/ants/js/entities/ant.js
+++ games/ants/js/entities/ant.js
@@ -311,7 +311,7 @@ const Colony = extend(EntityGroup, {
   symbol:Symbol("Colony"),
   colonies:(new Set()),
   entityType:Ant,
-  init( nest = this.nest,color = this.color,goals = this.goals,decay = 0.01,colonies = this.colonies,foodWeights = create(Pheremones)(color, decay, sim.layers.get()),matingWeights = create(Pheremones)({ 
+  init( nest = this.nest,color = this.color,goals = this.goals,decay = 0.001,colonies = this.colonies,foodWeights = create(Pheremones)(color, decay, sim.layers.get()),matingWeights = create(Pheremones)({ 
     red:color.red,
     green:color.green,
     blue:255
