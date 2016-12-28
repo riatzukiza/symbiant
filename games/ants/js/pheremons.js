2d3c987a7e39878d1c03e4f1369485dfaf6804a2
Your Name <you@example.com>
2016-12-28 17:26:29 -0600

    compiled /home/aaron/devel/symbiant/games/ants/sibilant/js/pheremons.sibilant

--- games/ants/js/pheremons.js
+++ games/ants/js/pheremons.js
@@ -146,6 +146,7 @@ const Pheremones = {
     
       this.color = color;this.decay = decay;this.decaying = decaying;this.weights = weights;this.id = id;this.lastUpdate = lastUpdate;
       ++(Pheremones.id);
+      addMixingLayer(this, weights);
       return this;
     
    },
