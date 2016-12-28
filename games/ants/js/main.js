9fd8460f33bc214281e7ec483dbea4b51c23e02c
Your Name <you@example.com>
2016-12-28 14:11:05 -0600

    compiled /home/aaron/devel/symbiant/games/ants/sibilant/js/main.sibilant

--- games/ants/js/main.js
+++ games/ants/js/main.js
@@ -17,7 +17,7 @@ const {
 const { 
   Simulation
  } = require("./simulation");
-let sim = create(Simulation)(120, 120, 5);
+let sim = create(Simulation)(120, 240, 2);
 global.sim = sim;
 const Rendering = { 
   symbol:Symbol("Rendering")
