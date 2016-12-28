2cdeac8cf0be4435a74a86e9857e62ec64ae60c7
Your Name <you@example.com>
2016-12-28 12:07:27 -0600

    compiled /home/aaron/devel/symbiant/games/ants/sibilant/js/entities/ant.sibilant

--- games/ants/js/entities/ant.js
+++ games/ants/js/entities/ant.js
@@ -287,7 +287,7 @@ const Ant = extend(Entity, {
               return (function() {
                 if (ant._nearNest()) {
                   return ant._reproduce();
-                } else if ((group.colonies.size < 30 && Math.random() > 0.9999)) {
+                } else if ((group.colonies.size < 30 && Math.random() > 0.9999999)) {
                   return ant._formNewColony();
                 }
               }).call(this);
