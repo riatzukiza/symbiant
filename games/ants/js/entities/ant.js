b5673b209c694efe1718266dacdb1078c1dfce21
Aaron Beavers <foamy125@gmail.com>
2022-05-02 17:21:34 -0400

    compiled /home/aaronb/devel/err-studios/symbiant/games/ants/sibilant/js/entities/ant.sibilant

--- games/ants/js/entities/ant.js
+++ games/ants/js/entities/ant.js
@@ -436,7 +436,7 @@ const Ant = extend(Entity, {
               return (function() {
                 if (this._nearNest()) {
                   return this._reproduce();
-                } else if (Math.random() > 0.1) {
+                } else if (Math.random() > 0.9999) {
                   return this._formNewColony();
                 }
               }).call(this);
