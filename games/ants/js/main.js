9c2362a54bd34c11681cc969044cd847b7ef5d68
Aaron Beavers <foamy125@gmail.com>
2022-05-02 16:16:55 -0400

    compiled /home/aaronb/devel/err-studios/symbiant/games/ants/sibilant/js/main.sibilant

--- games/ants/js/main.js
+++ games/ants/js/main.js
@@ -18,7 +18,7 @@ const {
 const { 
   Simulation
  } = require("./simulation");
-let sim = create(Simulation)(300, 300, 5);
+let sim = create(Simulation)(300, 300, 4);
 global.sim = sim;
 const Rendering = { 
   symbol:Symbol("Rendering")
