5246afefebacfe4626ffd661be8c0a4ebe35cecd
Aaron Beavers <foamy125@gmail.com>
2022-05-02 20:15:31 -0400

    compiled /home/aaronb/devel/err-studios/symbiant/games/ants/sibilant/js/pheremons.sibilant

--- games/ants/js/pheremons.js
+++ games/ants/js/pheremons.js
@@ -191,7 +191,7 @@ const Pheremones = {
         }).call(this);
         return (function() {
           if (w < 1) {
-            return coord.layers[id] += ((0.5 * rate) / (0.9 + Math.pow(euclidianDistance(x, y, pos.x, pos.y), 2)));
+            return coord.layers[id] += ((0.5 * rate) / (0.3 + Math.pow(euclidianDistance(x, y, pos.x, pos.y), 2)));
           }
         }).call(this);
       
