60f8f2d628de9385d3bfa8ec2c80a0e2d325e762
Aaron Beavers <foamy125@gmail.com>
2022-05-04 18:19:01 -0400

    compiled /home/aaronb/devel/err-studios/symbiant/games/engine/sibilant/js/main.sibilant

--- games/engine/js/main.js
+++ games/engine/js/main.js
@@ -244,9 +244,9 @@ game.events.on("collision", (([ c, c_, d ]) => {
   var c_v = game.systems.get(Velocity, c_.entity);
   console.log("cv.id", cv.id);
   console.log("cv.xd", cv.xd);
+  console.log("cv.yd", cv.yd);
   console.log("c_v.id", c_v.id);
   console.log("c_v.xd", c_v.xd);
-  console.log("cv.yd", cv.yd);
   console.log("c_v.yd", c_v.yd);
   cv.xd = (cv.xd + c_v.xd);
   cv.yd = (cv.yd + c_.yd);
