b947086c8f84a0e43cb379247b705f27d56cb876
Aaron Beavers <foamy125@gmail.com>
2022-05-04 17:04:22 -0400

    compiled /home/aaronb/devel/err-studios/symbiant/games/engine/sibilant/js/main.sibilant

--- games/engine/js/main.js
+++ games/engine/js/main.js
@@ -246,8 +246,8 @@ game.systems.get(Dot, dot).color = rgba(255, 0, 0, 255);
   $for = (function() {
     /* node_modules/kit/inc/loops.sibilant:28:35 */
   
-    game.systems.get(Position, dot).x = (Math.floor((Math.random() * (1000 - 0))) + 0);
-    game.systems.get(Position, dot).y = (Math.floor((Math.random() * (1000 - 0))) + 0);
+    game.systems.get(Position, dot).x = (Math.floor((Math.random() * (1000 - 1))) + 1);
+    game.systems.get(Position, dot).y = (Math.floor((Math.random() * (1000 - 1))) + 1);
     game.systems.get(Position, dot).z = 1;
     game.systems.get(Physics, dot).scale = 10;
     game.systems.get(Physics, dot).mass = 10;
