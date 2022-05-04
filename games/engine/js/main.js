4c7424a1b06b9f6d366daa44fbfe7ef1b8cea864
Aaron Beavers <foamy125@gmail.com>
2022-05-04 18:52:55 -0400

    compiled /home/aaronb/devel/err-studios/symbiant/games/engine/sibilant/js/main.sibilant

--- games/engine/js/main.js
+++ games/engine/js/main.js
@@ -254,10 +254,10 @@ game.events.on("collision", (([ c, c_, d ]) => {
   var m = ((2 * cp.mass) / (c_p.mass + cp.mass));
   var m_ = ((2 * c_p.mass) / (c_p.mass + cp.mass));
   var v = undefined;
-  cv.xd = (m * (cv.xd - c_v.xd));
-  cv.yd = (m * (cv.yd - c_v.yd));
-  c_v.xd = (m_ * (c_v.xd - cv.xd));
-  c_v.yd = (m_ * (c_v.yd - cv.yd));
+  cv.xd = (m_ * (cv.xd - c_v.xd));
+  cv.yd = (m_ * (cv.yd - c_v.yd));
+  c_v.xd = (m * (c_v.xd - cv.xd));
+  c_v.yd = (m * (c_v.yd - cv.yd));
   console.log("After collision");
   console.log("cv.id", cv.id);
   console.log("cv.xd", cv.xd);
