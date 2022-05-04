891f367bdb5790a7df74b951aeaaf479204361d9
Aaron Beavers <foamy125@gmail.com>
2022-05-04 19:03:23 -0400

    compiled /home/aaronb/devel/err-studios/symbiant/games/engine/sibilant/js/main.sibilant

--- games/engine/js/main.js
+++ games/engine/js/main.js
@@ -10,6 +10,18 @@ var {
 var { 
   Interface
  } = require("@kit-js/interface");
+var socket = io("/engine");
+socket.on("change", (() => {
+	
+  console.log("change ");
+  return location.reload();
+
+})).once("error", ((err) => {
+	
+  console.log("error on", "change", "of", "socket", "given", "null");
+  return console.log(err);
+
+}));
 global.mixin = mixin;
 global.create = create;
 var { 
@@ -192,22 +204,22 @@ var activeGameSystems = [ Dot, Position, Physics, Velocity, Collision ];
 var game = create(Game)(rendering, activeGameSystems);
 game.start();
 var entity = (function entity$(aspects) {
-  /* entity eval.sibilant:111:0 */
+  /* entity eval.sibilant:114:0 */
 
   return game.ent.spawn(aspects);
 });
 var vector2d = (function vector2d$(x, y) {
-  /* vector2d eval.sibilant:112:0 */
+  /* vector2d eval.sibilant:115:0 */
 
   return [ x, y ];
 });
 TreeMap.get = (function TreeMap$get$(...args) {
-  /* Tree-map.get eval.sibilant:114:0 */
+  /* Tree-map.get eval.sibilant:117:0 */
 
   return this.find(...args).value;
 });
 var memoize = (function memoize$(f) {
-  /* memoize eval.sibilant:116:0 */
+  /* memoize eval.sibilant:119:0 */
 
   var cache = create(TreeMap)();
   return ((...args) => {
@@ -217,7 +229,7 @@ var memoize = (function memoize$(f) {
         return cache.get(args);
       } else {
         var r = (function() {
-          /* eval.sibilant:36:23 */
+          /* eval.sibilant:39:23 */
         
           return f(...args);
         }).call(this);
@@ -283,15 +295,3 @@ game.events.on("collision", (([ c, c_, d ]) => {
   ;
   return $for;
 }).call(this);
-var socket = io("/engine");
-socket.on("change", (() => {
-	
-  console.log("change ");
-  return location.reload();
-
-})).once("error", ((err) => {
-	
-  console.log("error on", "change", "of", "socket", "given", "null");
-  return console.log(err);
-
-}));
