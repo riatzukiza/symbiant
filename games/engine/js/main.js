796ef2857f70b4a251d925c1a5c03ec64766bb86
Aaron Beavers <foamy125@gmail.com>
2022-05-05 11:50:16 -0400

    compiled /home/aaronb/devel/err-studios/symbiant/games/engine/sibilant/js/main.sibilant

--- games/engine/js/main.js
+++ games/engine/js/main.js
@@ -208,8 +208,8 @@ var Friction = Physics.Force.define("Friction", {
       var collision = c.system.process.systems.get(Collision, c.entity);
       return (function() {
         if (!(collision.colliding)) {
-          v.xd += (-1 * (v.xd / 640));
-          return v.yd += (-1 * (v.yd / 640));
+          v.xd += (-1 * (v.xd / 32));
+          return v.yd += (-1 * (v.yd / 32));
         }
       }).call(this);
     
