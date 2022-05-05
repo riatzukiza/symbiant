fc5fbf1295823ea3fe58d92232cee684cf50171b
Aaron Beavers <foamy125@gmail.com>
2022-05-05 10:07:51 -0400

    compiled /home/aaronb/devel/err-studios/symbiant/games/engine/sibilant/js/config.sibilant

--- games/engine/js/config.js
+++ games/engine/js/config.js
@@ -1,8 +1,9 @@
+const dim=[ 200200 ];
 module.exports.height = 100;
 module.exports.width = 100;
 module.exports.size = 5;
 module.exports.angleZoom = 100;
 module.exports.noiseZ = 100;
 module.exports.fieldForce = 100;
-module.exports.dimensions = [ 200200 ];
+module.exports.dimensions = dim;
 module.exports.columns = (config.dimensions[0] / config.size);
