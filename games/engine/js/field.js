a140711dfcd75be7a073c32b32bbe4841d6e1045
Aaron Beavers <foamy125@gmail.com>
2022-05-05 15:25:19 -0400

    compiled /home/aaronb/devel/err-studios/symbiant/games/engine/sibilant/js/main.sibilant

--- games/engine/js/field.js
+++ games/engine/js/field.js
@@ -18,7 +18,7 @@ module.exports.updateParticle = function updateParticle(vel,p,field,pheremones,t
     let vec = field[pos.x][pos.y];
 
 
-    if(vec.getLength() < 5) {
+    if(vec.getLength() < 10) {
     pheremones[pos.x][pos.y].addTo(vec)
 
     vec = pheremones[pos.x][pos.y];
