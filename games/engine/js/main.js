906d932f532720172ddde30d1f570c545449a764
Aaron Beavers <foamy125@gmail.com>
2022-05-05 10:35:17 -0400

    compiled /home/aaronb/devel/err-studios/symbiant/games/engine/sibilant/js/main.sibilant

--- games/engine/js/main.js
+++ games/engine/js/main.js
@@ -335,7 +335,7 @@ var SignalField = Physics.Force.define("SignalField", {
     game.systems.get(Position, dot).z = 1;
     game.systems.get(Physics, dot).scale = 10;
     game.systems.get(Physics, dot).mass = 1;
-    return game.systems.get(Physics, dot).forces = [ SignalField ];
+    return game.systems.get(Physics, dot).forces = [ SignalField, Friction ];
   }).call(this);
   }
   ;
