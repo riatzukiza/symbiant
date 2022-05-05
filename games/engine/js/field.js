52659d354b781328942e0d6164f4202f2c6c6bdc
Aaron Beavers <foamy125@gmail.com>
2022-05-05 19:39:55 -0400

    compiled /home/aaronb/devel/err-studios/symbiant/games/engine/sibilant/js/config.sibilant

--- games/engine/js/field.js
+++ games/engine/js/field.js
@@ -31,11 +31,11 @@ module.exports.updateParticle = function updateParticle(vel,p,field,pheremones,t
 
 
     pH.addTo(vec)
-    vel.accelerate([pH.x,pH.y]);
+    vel.accelerate([pH.x/10,pH.y/10]);
 
     pH.addTo({
-      x:vel.xd,
-      y:vel.yd
+      x:vel.xd/10,
+      y:vel.yd/10
 
     })
     // console.log({tick,vec,vel})
