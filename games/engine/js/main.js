88611e2c9ae36b944b4f165fde8e52731ca5eb80
Aaron Beavers <foamy125@gmail.com>
2022-05-06 16:37:53 -0400

    compiled /home/aaronb/devel/err-studios/symbiant/games/engine/sibilant/js/main.sibilant

--- games/engine/js/main.js
+++ games/engine/js/main.js
@@ -299,13 +299,13 @@ game.events.on("collision", (([ c, c_, d ]) => {
       cv.accelerate([ (function() {
         /* eval.sibilant:29:8 */
       
-        var rand = ((Math.random() * ((c_v.xd * config.collisionStatic) - 0)) + 0);
-        return ((c_v.xd * config.collisionStatic) - (rand * 2));
+        var rand = ((Math.random() * (((1 + c_v.xd) * config.collisionStatic) - 0)) + 0);
+        return (((1 + c_v.xd) * config.collisionStatic) - (rand * 2));
       }).call(this), (function() {
         /* eval.sibilant:29:8 */
       
-        var rand = ((Math.random() * ((c_v.yd * config.collisionStatic) - 0)) + 0);
-        return ((c_v.yd * config.collisionStatic) - (rand * 2));
+        var rand = ((Math.random() * (((1 + c_v.yd) * config.collisionStatic) - 0)) + 0);
+        return (((1 + c_v.yd) * config.collisionStatic) - (rand * 2));
       }).call(this) ]);
       c.colliding = false;
       c_.colliding = false;
@@ -408,11 +408,11 @@ game.systems.get(Physics, target).scale = 40;
 game.systems.get(Physics, target).mass = 10000;
 game.systems.get(Physics, target).forces = [ Friction ];
 const ants=[];
-var spawnAnt = (function spawnAnt$(x_y$24, home, startingLife) {
+var spawnAnt = (function spawnAnt$(x_y$25, home, startingLife) {
   /* spawn-ant eval.sibilant:254:0 */
 
-  var x = x_y$24[0],
-      y = x_y$24[1];
+  var x = x_y$25[0],
+      y = x_y$25[1];
 
   var ant = entity(activeGameSystems);
   ants.push(ant);
