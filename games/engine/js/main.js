ccaad60c82c215d5abae270282049ba08b241d4b
Aaron Beavers <foamy125@gmail.com>
2022-05-06 19:43:26 -0400

    compiled /home/aaronb/devel/err-studios/symbiant/games/engine/sibilant/js/main.sibilant

--- games/engine/js/main.js
+++ games/engine/js/main.js
@@ -356,11 +356,11 @@ game.systems.get(Physics, target).scale = 40;
 game.systems.get(Physics, target).mass = 10000;
 game.systems.get(Physics, target).forces = [ Friction ];
 const ants=[];
-var spawnAnt = (function spawnAnt$(x_y$20, home, startingLife) {
+var spawnAnt = (function spawnAnt$(x_y$21, home, startingLife) {
   /* spawn-ant eval.sibilant:261:0 */
 
-  var x = x_y$20[0],
-      y = x_y$20[1];
+  var x = x_y$21[0],
+      y = x_y$21[1];
 
   var ant = entity(activeGameSystems);
   ants.push(ant);
@@ -433,12 +433,12 @@ settings.addRange("Min Trail", 0, 9999, ((val) => {
   return config.minTrail = val;
 
 }));
-settings.addBoolean("Decay on collision", 0, 9999, ((val) => {
+settings.addBoolean("Decay on collision", ((val) => {
 	
-  return config.maxLength = val;
+  return config.decayOnCollision = val;
 
 }));
-settings.addBoolean("Limit the number of decay blocks per cycle", 0, 9999, ((val) => {
+settings.addBoolean("Limit the number of decay blocks per cycle", ((val) => {
 	
   return config.maxLength = val;
 
