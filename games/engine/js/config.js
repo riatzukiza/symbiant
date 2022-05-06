a5e7a63dea83b6ac53ea31f51f603456b5cd8385
Aaron Beavers <foamy125@gmail.com>
2022-05-06 19:56:13 -0400

    compiled /home/aaronb/devel/err-studios/symbiant/games/engine/sibilant/js/config.sibilant

--- games/engine/js/config.js
+++ games/engine/js/config.js
@@ -19,4 +19,4 @@ module.exports.antInfluence = 10.8;
 module.exports.friction = 8.5;
 module.exports.collisionStatic = 10;
 module.exports.spawnStatic = 10;
-module.exports.spawnRate = 10000;
+module.exports.spawnRate = 100;
