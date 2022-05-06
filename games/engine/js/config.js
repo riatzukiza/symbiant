d807bbeb3e119d2b45ea7ca121caa51b847171d3
Aaron Beavers <foamy125@gmail.com>
2022-05-06 10:42:28 -0400

    compiled /home/aaronb/devel/err-studios/symbiant/games/engine/sibilant/js/config.sibilant

--- games/engine/js/config.js
+++ games/engine/js/config.js
@@ -1,14 +1,14 @@
 const dim=[ window.innerWidth, window.innerHeight ];
 const size=5;
 module.exports.size = size;
-module.exports.angleZoom = 1;
-module.exports.noiseZ = 1;
+module.exports.angleZoom = 10;
+module.exports.noiseZ = 10;
 module.exports.fieldForce = 100;
 module.exports.dimensions = dim;
 module.exports.columns = Math.ceil((dim[0] / size));
 module.exports.rows = Math.ceil((dim[1] / size));
 module.exports.decay = 0.4;
-module.exports.maxLength = 10;
+module.exports.maxLength = 100;
 module.exports.antLimit = 200;
 module.exports.maxInDecay = 50;
 module.exports.maxTrail = 100;
