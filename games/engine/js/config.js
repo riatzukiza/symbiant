3769b79203b5818b0315bb9857f2a8d3c4b5704b
Aaron Beavers <foamy125@gmail.com>
2022-05-06 12:44:32 -0400

    compiled /home/aaronb/devel/err-studios/symbiant/games/engine/sibilant/js/config.sibilant

--- games/engine/js/config.js
+++ games/engine/js/config.js
@@ -15,5 +15,5 @@ module.exports.maxTrail = 400;
 module.exports.minTrail = 100;
 module.exports.limitDecay = false;
 module.exports.antInfluence = 10;
-module.exports.friction = 7.8;
+module.exports.friction = 3.8;
 module.exports.spawnRate = 100;
