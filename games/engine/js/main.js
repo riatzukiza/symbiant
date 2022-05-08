0b112a0492190a39e448f5261dd85e1aa731bda4
Aaron Beavers <foamy125@gmail.com>
2022-05-07 20:24:39 -0400

    compiled /home/aaronb/devel/err-studios/symbiant/games/engine/sibilant/js/main.sibilant

--- games/engine/js/main.js
+++ games/engine/js/main.js
@@ -231,22 +231,22 @@ List.rotateUntil = (function List$rotateUntil$(predicate = this.predicate, t = 0
   }).call(this);
 });
 var entity = (function entity$(aspects) {
-  /* entity eval.sibilant:75:0 */
+  /* entity eval.sibilant:73:0 */
 
   return game.ent.spawn(aspects);
 });
 var vector2d = (function vector2d$(x, y) {
-  /* vector2d eval.sibilant:76:0 */
+  /* vector2d eval.sibilant:74:0 */
 
   return [ x, y ];
 });
 TreeMap.get = (function TreeMap$get$(...args) {
-  /* Tree-map.get eval.sibilant:78:0 */
+  /* Tree-map.get eval.sibilant:76:0 */
 
   return this.find(...args).value;
 });
 var memoize = (function memoize$(f) {
-  /* memoize eval.sibilant:80:0 */
+  /* memoize eval.sibilant:78:0 */
 
   var cache = create(TreeMap)();
   return ((...args) => {
@@ -299,11 +299,11 @@ game.systems.get(Physics, target).scale = 40;
 game.systems.get(Physics, target).mass = 10000;
 game.systems.get(Physics, target).forces = [ Friction ];
 const ants=[];
-var spawnAnt = (function spawnAnt$(x_y$6, home, startingLife) {
-  /* spawn-ant eval.sibilant:134:0 */
+var spawnAnt = (function spawnAnt$(x_y$7, home, startingLife) {
+  /* spawn-ant eval.sibilant:132:0 */
 
-  var x = x_y$6[0],
-      y = x_y$6[1];
+  var x = x_y$7[0],
+      y = x_y$7[1];
 
   var ant = entity(activeGameSystems);
   ants.push(ant);
@@ -342,7 +342,7 @@ var nextSpawn = (() => {
 
 });
 var clearAnts = (function clearAnts$() {
-  /* clear-ants eval.sibilant:163:0 */
+  /* clear-ants eval.sibilant:161:0 */
 
   console.log("clearning ants", ants);
   ants.each(((ant) => {
