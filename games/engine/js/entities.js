e8598e59511df6c446663fe4e37f03a8bb5e5329
Aaron Beavers <foamy125@gmail.com>
2022-05-07 21:55:37 -0400

    compiled /home/aaronb/devel/err-studios/symbiant/games/engine/sibilant/js/entities.sibilant

--- games/engine/js/entities.js
+++ games/engine/js/entities.js
@@ -27,8 +27,8 @@ var {
   System
  } = require("sibilant-game-engine/client/ecs/component"),
     { 
-  EntityGroup
- } = require("sibilant-game-engine/client/ecs/entity"),
+  Group
+ } = require("sibilant-game-engine/client/datastructures/group"),
     { 
   Collision
  } = require("sibilant-game-engine/client/systems/collision"),
@@ -132,11 +132,11 @@ game.systems.get(Physics, target).scale = 40;
 game.systems.get(Physics, target).mass = 10000;
 game.systems.get(Physics, target).forces = [ Friction ];
 const ants=create(EntityGroup)("Ants", activeGameSystems, game.ent);
-var spawnAnt = (function spawnAnt$(x_y$6, home, startingLife) {
+var spawnAnt = (function spawnAnt$(x_y$7, home, startingLife) {
   /* spawn-ant eval.sibilant:98:0 */
 
-  var x = x_y$6[0],
-      y = x_y$6[1];
+  var x = x_y$7[0],
+      y = x_y$7[1];
 
   var ant = ants.spawn(activeGameSystems);
   game.systems.get(Dot, ant).color = rgba(255, 0, 0, 255);
