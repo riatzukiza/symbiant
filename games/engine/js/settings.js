50372f5d26487e86cc1f2589f51e1c83de2ed2d2
Aaron Beavers <foamy125@gmail.com>
2022-05-07 21:34:21 -0400

    compiled /home/aaronb/devel/err-studios/symbiant/games/engine/sibilant/js/settings.sibilant

--- games/engine/js/settings.js
+++ games/engine/js/settings.js
@@ -70,7 +70,7 @@ settings.addRange("Spawn Rate", 1, 99999, config.spawnRate, 1, ((val) => {
   return config.spawnRate = val;
 
 }));
-settings.addRange("Game speed", 0.1, 5, config.spawnRate, 0.1, ((val) => {
+settings.addRange("Game speed", 0.1, 5, config.gameSpeed, 0.1, ((val) => {
 	
   return config.gameSpeed = val;
 
