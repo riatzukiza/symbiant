9951637578e99fad21f0d6135a4b27b96694e0da
Aaron Beavers <foamy125@gmail.com>
2022-05-08 13:29:58 -0400

    compiled /home/aaronb/devel/err-studios/symbiant/games/engine/sibilant/js/config.sibilant

--- games/engine/js/config.js
+++ games/engine/js/config.js
@@ -1,5 +1,5 @@
-const dim = [ window.innerWidth, window.innerHeight ];
-const size = 25;
+const dim=[ window.innerWidth, window.innerHeight ];
+const size=25;
 module.exports.size = size;
 module.exports.angleZoom = 30;
 module.exports.noiseZ = 70;
@@ -16,7 +16,7 @@ module.exports.minTrail = 10;
 module.exports.decayOnCollision = false;
 module.exports.limitDecay = false;
 module.exports.antInfluence = 10;
-module.exports.friction = 64;
+module.exports.friction = 4;
 module.exports.collisionStatic = 1;
 module.exports.spawnStatic = 1;
 module.exports.spawnRate = 1000;
