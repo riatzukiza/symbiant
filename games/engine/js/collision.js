c133ca745b7e31b6d59dc961cc71077f310454c7
Aaron Beavers <foamy125@gmail.com>
2022-05-08 01:42:53 -0400

    compiled /home/aaronb/devel/err-studios/symbiant/games/engine/sibilant/js/collision.sibilant

--- games/engine/js/collision.js
+++ games/engine/js/collision.js
@@ -25,6 +25,7 @@ game.events.on("collision", ([ c, c_, d ]) => {
   var c_v = game.systems.get(Velocity, c_.entity);
   var cp = game.systems.get(Physics, c.entity);
   var c_p = game.systems.get(Physics, c_.entity);
+  console.log("collision");
   return (function() {
     if (((c.entity === home && c_.entity === target) || (c_.entity === home && c.entity === target))) {
       return console.log("target colliding with spawn");
