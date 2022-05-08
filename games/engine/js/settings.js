c723866b8dfc91c06a5660c611b203a65732622e
Aaron Beavers <foamy125@gmail.com>
2022-05-07 21:59:08 -0400

    compiled /home/aaronb/devel/err-studios/symbiant/games/engine/sibilant/js/settings.sibilant

--- games/engine/js/settings.js
+++ games/engine/js/settings.js
@@ -76,5 +76,5 @@ settings.addRange("Game speed", 0.1, 5, config.gameSpeed, 0.1, ((val) => {
   return config.gameSpeed = val;
 
 }));
-settings.addButton("Clear ants", clearAnts);
+settings.addButton("Clear ants", entities.clearAnts);
 module.exports = settings;
