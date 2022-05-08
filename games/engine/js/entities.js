ed1fa7e0ced8462d0246be2849d419f4061a498a
Aaron Beavers <foamy125@gmail.com>
2022-05-07 21:55:04 -0400

    compiled /home/aaronb/devel/err-studios/symbiant/games/engine/sibilant/js/entities.sibilant

--- games/engine/js/entities.js
+++ games/engine/js/entities.js
@@ -8,6 +8,9 @@ var {
   partiallyApplyAfter
  } = require("@kit-js/core/js/util");
 var { 
+  Interface
+ } = require("@kit-js/interface");
+var { 
   Dot
  } = require("sibilant-game-engine/client/systems/rendering/dot"),
     { 
@@ -43,13 +46,36 @@ var {
   TreeMap
  } = require("tree-kit"),
     config = require("./config");
+var EntityGroup = Interface.define("EntityGroup", { 
+  init( name = this.name,aspects = this.aspects,system = this.system,group = create(Group)() ){ 
+    
+      this.name = name;this.aspects = aspects;this.system = system;this.group = group;
+      return this;
+    
+   },
+  clear( group ){ 
+    
+      return group.each(clear);
+    
+   },
+  spawn( aspects = this.aspects,system = this.system,group = this.group ){ 
+    
+      return (function(e) {
+        /* node_modules/kit/inc/scope.sibilant:12:9 */
+      
+        group.add(e);
+        return e;
+      })(system.spawn(aspects));
+    
+   }
+ });
 TreeMap.get = (function TreeMap$get$(...args) {
-  /* Tree-map.get eval.sibilant:27:0 */
+  /* Tree-map.get eval.sibilant:42:0 */
 
   return this.find(...args).value;
 });
 var memoize = (function memoize$(f) {
-  /* memoize eval.sibilant:30:0 */
+  /* memoize eval.sibilant:45:0 */
 
   var cache = create(TreeMap)();
   return ((...args) => {
@@ -84,7 +110,7 @@ var rgba = memoize(((r, g, b, a) => {
 
 }));
 var entity = (function entity$(aspects) {
-  /* entity eval.sibilant:37:0 */
+  /* entity eval.sibilant:52:0 */
 
   return game.ent.spawn(aspects);
 });
@@ -106,11 +132,11 @@ game.systems.get(Physics, target).scale = 40;
 game.systems.get(Physics, target).mass = 10000;
 game.systems.get(Physics, target).forces = [ Friction ];
 const ants=create(EntityGroup)("Ants", activeGameSystems, game.ent);
-var spawnAnt = (function spawnAnt$(x_y$5, home, startingLife) {
-  /* spawn-ant eval.sibilant:83:0 */
+var spawnAnt = (function spawnAnt$(x_y$6, home, startingLife) {
+  /* spawn-ant eval.sibilant:98:0 */
 
-  var x = x_y$5[0],
-      y = x_y$5[1];
+  var x = x_y$6[0],
+      y = x_y$6[1];
 
   var ant = ants.spawn(activeGameSystems);
   game.systems.get(Dot, ant).color = rgba(255, 0, 0, 255);
@@ -122,12 +148,12 @@ var spawnAnt = (function spawnAnt$(x_y$5, home, startingLife) {
   game.systems.get(Physics, ant).forces = [ SignalField, Friction ];
   var v = game.systems.get(Velocity, ant);
   v.accelerate([ (function() {
-    /* eval.sibilant:21:8 */
+    /* eval.sibilant:36:8 */
   
     var rand = ((Math.random() * (config.spawnStatic - 0)) + 0);
     return (config.spawnStatic - (rand * 2));
   }).call(this), (function() {
-    /* eval.sibilant:21:8 */
+    /* eval.sibilant:36:8 */
   
     var rand = ((Math.random() * (config.spawnStatic - 0)) + 0);
     return (config.spawnStatic - (rand * 2));
