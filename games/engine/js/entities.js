64bc37d71a4f2027a186020fd118fc93dbaa849f
Aaron Beavers <foamy125@gmail.com>
2022-05-07 20:52:53 -0400

    compiled /home/aaronb/devel/err-studios/symbiant/games/engine/sibilant/js/entities.sibilant

--- games/engine/js/entities.js
+++ games/engine/js/entities.js
@@ -45,68 +45,66 @@ var {
   Friction
  } = require("./forces/friction"),
     config = require("./config");
-var setupEntities = (function setupEntities$(game) {
-  /* setup-entities eval.sibilant:20:0 */
+var home = entity([ Dot, Position, Physics, Collision ]);
+var homePos = game.systems.get(Position, home);
+game.systems.get(Dot, home).color = rgba(0, 255, 0, 255);
+game.systems.get(Position, home).x = (Math.floor((Math.random() * (config.dimensions[0] - 1))) + 1);
+game.systems.get(Position, home).y = (Math.floor((Math.random() * (config.dimensions[1] - 1))) + 1);
+game.systems.get(Position, home).z = 1;
+game.systems.get(Physics, home).scale = 30;
+game.systems.get(Physics, home).mass = 1;
+game.systems.get(Physics, home).forces = [];
+var target = entity([ Dot, Position, Physics, Collision, Velocity ]);
+game.systems.get(Dot, target).color = rgba(0, 0, 255, 255);
+game.systems.get(Position, target).x = (Math.floor((Math.random() * (config.dimensions[0] - 1))) + 1);
+game.systems.get(Position, target).y = (Math.floor((Math.random() * (config.dimensions[1] - 1))) + 1);
+game.systems.get(Position, target).z = 1;
+game.systems.get(Physics, target).scale = 40;
+game.systems.get(Physics, target).mass = 10000;
+game.systems.get(Physics, target).forces = [ Friction ];
+const ants = [];
+var spawnAnt = (function spawnAnt$(x_y$5, home, startingLife) {
+  /* spawn-ant eval.sibilant:61:0 */
 
-  var home = entity([ Dot, Position, Physics, Collision ]);
-  var homePos = game.systems.get(Position, home);
-  game.systems.get(Dot, home).color = rgba(0, 255, 0, 255);
-  game.systems.get(Position, home).x = (Math.floor((Math.random() * (config.dimensions[0] - 1))) + 1);
-  game.systems.get(Position, home).y = (Math.floor((Math.random() * (config.dimensions[1] - 1))) + 1);
-  game.systems.get(Position, home).z = 1;
-  game.systems.get(Physics, home).scale = 30;
-  game.systems.get(Physics, home).mass = 1;
-  game.systems.get(Physics, home).forces = [];
-  var target = entity([ Dot, Position, Physics, Collision, Velocity ]);
-  game.systems.get(Dot, target).color = rgba(0, 0, 255, 255);
-  game.systems.get(Position, target).x = (Math.floor((Math.random() * (config.dimensions[0] - 1))) + 1);
-  game.systems.get(Position, target).y = (Math.floor((Math.random() * (config.dimensions[1] - 1))) + 1);
-  game.systems.get(Position, target).z = 1;
-  game.systems.get(Physics, target).scale = 40;
-  game.systems.get(Physics, target).mass = 10000;
-  game.systems.get(Physics, target).forces = [ Friction ];
-  const ants = [];
-  var spawnAnt = (function spawnAnt$(x_y$3, home, startingLife) {
-    /* spawn-ant eval.sibilant:63:5 */
+  var x = x_y$5[0],
+      y = x_y$5[1];
+
+  var ant = entity(activeGameSystems);
+  ants.push(ant);
+  console.log(ant);
+  game.systems.get(Dot, ant).color = rgba(255, 0, 0, 255);
+  game.systems.get(Position, ant).x = x;
+  game.systems.get(Position, ant).y = y;
+  game.systems.get(Position, ant).z = 1;
+  game.systems.get(Physics, ant).scale = 10;
+  game.systems.get(Physics, ant).mass = 1;
+  game.systems.get(Physics, ant).forces = [ SignalField, Friction ];
+  var v = game.systems.get(Velocity, ant);
+  v.accelerate([ (function() {
+    /* eval.sibilant:33:8 */
   
-    var x = x_y$3[0],
-        y = x_y$3[1];
+    let rand = ((Math.random() * (config.spawnStatic - 0)) + 0);
+    return (config.spawnStatic - (rand / 2));
+  }).call(this), (function() {
+    /* eval.sibilant:33:8 */
   
-    var ant = entity(activeGameSystems);
-    ants.push(ant);
-    console.log(ant);
-    game.systems.get(Dot, ant).color = rgba(255, 0, 0, 255);
-    game.systems.get(Position, ant).x = x;
-    game.systems.get(Position, ant).y = y;
-    game.systems.get(Position, ant).z = 1;
-    game.systems.get(Physics, ant).scale = 10;
-    game.systems.get(Physics, ant).mass = 1;
-    game.systems.get(Physics, ant).forces = [ SignalField, Friction ];
-    var v = game.systems.get(Velocity, ant);
-    v.accelerate([ (function() {
-      /* eval.sibilant:33:8 */
-    
-      let rand = ((Math.random() * (config.spawnStatic - 0)) + 0);
-      return (config.spawnStatic - (rand / 2));
-    }).call(this), (function() {
-      /* eval.sibilant:33:8 */
-    
-      let rand = ((Math.random() * (config.spawnStatic - 0)) + 0);
-      return (config.spawnStatic - (rand / 2));
-    }).call(this) ]);
-    return ant;
-  });
-  var homePos = game.systems.get(Position, home);
-  var number = 1;
-  var nextSpawn = () => {
-  	
-    return (function() {
-      if (!(ants.length >= config.antLimit)) {
-        spawnAnt([ homePos.x, homePos.y ], home);
-        return setTimeout(nextSpawn, config.spawnRate);
-      }
-    }).call(this);
-  
-  };
-  return nextSpawn();
+    let rand = ((Math.random() * (config.spawnStatic - 0)) + 0);
+    return (config.spawnStatic - (rand / 2));
+  }).call(this) ]);
+  return ant;
 });
+var homePos = game.systems.get(Position, home);
+var number = 1;
+var nextSpawn = () => {
+	
+  return (function() {
+    if (!(ants.length >= config.antLimit)) {
+      spawnAnt([ homePos.x, homePos.y ], home);
+      return setTimeout(nextSpawn, config.spawnRate);
+    }
+  }).call(this);
+
+};
+exports.homePos = homePos;
+exports.nextSpawn = nextSpawn;
+nextSpawn();
