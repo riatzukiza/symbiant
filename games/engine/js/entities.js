4cb367c74e02702897fa5da76b8869353ad2a629
Aaron Beavers <foamy125@gmail.com>
2022-05-07 23:30:01 -0400

    compiled /home/aaronb/devel/err-studios/symbiant/games/engine/sibilant/js/entities.sibilant

--- games/engine/js/entities.js
+++ games/engine/js/entities.js
@@ -138,15 +138,15 @@ game.systems.get(Dot, target).color = rgba(0, 0, 255, 255);
 game.systems.get(Position, target).x = (Math.floor((Math.random() * (config.dimensions[0] - 1))) + 1);
 game.systems.get(Position, target).y = (Math.floor((Math.random() * (config.dimensions[1] - 1))) + 1);
 game.systems.get(Position, target).z = 1;
-game.systems.get(Physics, target).scale = 40;
+game.systems.get(Physics, target).scale = 70;
 game.systems.get(Physics, target).mass = 10000;
 game.systems.get(Physics, target).forces = [];
 const ants=create(EntityGroup)("Ants", activeGameSystems, game.ent);
-var spawnAnt = (function spawnAnt$(x_y$17, home, startingLife) {
+var spawnAnt = (function spawnAnt$(x_y$18, home, startingLife) {
   /* spawn-ant eval.sibilant:101:0 */
 
-  var x = x_y$17[0],
-      y = x_y$17[1];
+  var x = x_y$18[0],
+      y = x_y$18[1];
 
   var ant = ants.spawn(activeGameSystems);
   game.systems.get(Dot, ant).color = rgba(255, 0, 0, 255);
