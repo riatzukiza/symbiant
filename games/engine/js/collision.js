7a71df2b18818334e48cb82878372784f3fdc68d
Aaron Beavers <foamy125@gmail.com>
2022-05-08 12:52:12 -0400

    compiled /home/aaronb/devel/err-studios/symbiant/games/engine/sibilant/js/main.sibilant

--- games/engine/js/collision.js
+++ games/engine/js/collision.js
@@ -22,7 +22,7 @@ var {
   SignalField
  } = require("./forces/signal-field"),
     config = require("./config");
-game.events.on("collision", (([ c, c_, d ]) => {
+game.events.on("collision", ([ c, c_, d ]) => {
 	
   var cv = game.systems.get(Velocity, c.entity);
   var c_v = game.systems.get(Velocity, c_.entity);
@@ -41,15 +41,15 @@ game.events.on("collision", (([ c, c_, d ]) => {
       c_v.pos.x = homePos.x;
       c_v.pos.y = homePos.y;
       return c_v.accelerate([ (function() {
-        /* eval.sibilant:31:8 */
+        /* eval.sibilant:33:8 */
       
-        var rand = ((Math.random() * (config.collisionStatic - 0)) + 0);
-        return (config.collisionStatic - (rand * 2));
+        let rand = ((Math.random() * (config.collisionStatic - 0)) + 0);
+        return (config.collisionStatic - (rand / 2));
       }).call(this), (function() {
-        /* eval.sibilant:31:8 */
+        /* eval.sibilant:33:8 */
       
-        var rand = ((Math.random() * (config.collisionStatic - 0)) + 0);
-        return (config.collisionStatic - (rand * 2));
+        let rand = ((Math.random() * (config.collisionStatic - 0)) + 0);
+        return (config.collisionStatic - (rand / 2));
       }).call(this) ]);
     } else if (c_.entity === target) {
       console.log("ant found target");
@@ -57,46 +57,41 @@ game.events.on("collision", (([ c, c_, d ]) => {
       cv.pos.x = homePos.x;
       cv.pos.y = homePos.y;
       return cv.accelerate([ (function() {
-        /* eval.sibilant:31:8 */
+        /* eval.sibilant:33:8 */
       
-        var rand = ((Math.random() * (config.collisionStatic - 0)) + 0);
-        return (config.collisionStatic - (rand * 2));
+        let rand = ((Math.random() * (config.collisionStatic - 0)) + 0);
+        return (config.collisionStatic - (rand / 2));
       }).call(this), (function() {
-        /* eval.sibilant:31:8 */
+        /* eval.sibilant:33:8 */
       
-        var rand = ((Math.random() * (config.collisionStatic - 0)) + 0);
-        return (config.collisionStatic - (rand * 2));
+        let rand = ((Math.random() * (config.collisionStatic - 0)) + 0);
+        return (config.collisionStatic - (rand / 2));
       }).call(this) ]);
     } else if (!((c.entity === home || c_.entity === home || c.entity === target || c_.entity === target))) {
       console.log("ant is colliding with another ant");
       cv.accelerate([ (function() {
-        /* eval.sibilant:31:8 */
+        /* eval.sibilant:33:8 */
       
-        var rand = ((Math.random() * (config.collisionStatic - 0)) + 0);
-        return (config.collisionStatic - (rand * 2));
+        let rand = ((Math.random() * (config.collisionStatic - 0)) + 0);
+        return (config.collisionStatic - (rand / 2));
       }).call(this), (function() {
-        /* eval.sibilant:31:8 */
+        /* eval.sibilant:33:8 */
       
-        var rand = ((Math.random() * (config.collisionStatic - 0)) + 0);
-        return (config.collisionStatic - (rand * 2));
+        let rand = ((Math.random() * (config.collisionStatic - 0)) + 0);
+        return (config.collisionStatic - (rand / 2));
       }).call(this) ]);
       return c_v.accelerate([ (function() {
-        /* eval.sibilant:31:8 */
+        /* eval.sibilant:33:8 */
       
-        var rand = ((Math.random() * (config.collisionStatic - 0)) + 0);
-        return (config.collisionStatic - (rand * 2));
+        let rand = ((Math.random() * (config.collisionStatic - 0)) + 0);
+        return (config.collisionStatic - (rand / 2));
       }).call(this), (function() {
-        /* eval.sibilant:31:8 */
+        /* eval.sibilant:33:8 */
       
-        var rand = ((Math.random() * (config.collisionStatic - 0)) + 0);
-        return (config.collisionStatic - (rand * 2));
+        let rand = ((Math.random() * (config.collisionStatic - 0)) + 0);
+        return (config.collisionStatic - (rand / 2));
       }).call(this) ]);
     }
   }).call(this);
 
-})).once("error", ((err) => {
-	
-  console.log("error on", "collision", "of", "game.events", "given", "[ c, c_, d ]()");
-  return console.log(err);
-
-}));
+});
