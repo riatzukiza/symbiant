0165bf3f69d77108d943a01da895ef456dd47a1f
Aaron Beavers <foamy125@gmail.com>
2022-05-08 12:12:42 -0400

    compiled /home/aaronb/devel/err-studios/symbiant/games/engine/sibilant/js/entities.sibilant

--- games/engine/js/entities.js
+++ games/engine/js/entities.js
@@ -127,16 +127,16 @@ var entity = (function entity$(aspects) {
 var home = entity([ Dot, Position, Physics, Collision ]);
 var homePos = game.systems.get(Position, home);
 game.systems.get(Dot, home).color = rgba(0, 255, 0, 255);
-game.systems.get(Position, home).x = (Math.floor((Math.random() * (config.dimensions[0] - 1))) + 1);
-game.systems.get(Position, home).y = (Math.floor((Math.random() * (config.dimensions[1] - 1))) + 1);
+game.systems.get(Position, home).x = config.homeLocation[0];
+game.systems.get(Position, home).y = config.homeLocation[1];
 game.systems.get(Position, home).z = 1;
 game.systems.get(Physics, home).scale = 1;
 game.systems.get(Physics, home).mass = 1;
 game.systems.get(Physics, home).forces = [];
 var target = entity([ Dot, Position, Physics, Collision, Velocity ]);
 game.systems.get(Dot, target).color = rgba(0, 0, 255, 255);
-game.systems.get(Position, target).x = (Math.floor((Math.random() * (config.dimensions[0] - 1))) + 1);
-game.systems.get(Position, target).y = (Math.floor((Math.random() * (config.dimensions[1] - 1))) + 1);
+game.systems.get(Position, target).x = config.targetLocation[0];
+game.systems.get(Position, target).y = config.targetLocation[1];
 game.systems.get(Position, target).z = 1;
 game.systems.get(Physics, target).scale = 30;
 game.systems.get(Physics, target).mass = 10000;
@@ -146,11 +146,11 @@ game.systems.get(Collision, home).name = "home";
 target.name = "target";
 home.name = "home";
 const ants=create(EntityGroup)("Ants", activeGameSystems, game.ent);
-var spawnAnt = (function spawnAnt$(x_y$2, home, startingLife) {
+var spawnAnt = (function spawnAnt$(x_y$3, home, startingLife) {
   /* spawn-ant eval.sibilant:109:0 */
 
-  var x = x_y$2[0],
-      y = x_y$2[1];
+  var x = x_y$3[0],
+      y = x_y$3[1];
 
   var ant = ants.spawn(activeGameSystems);
   game.systems.get(Dot, ant).color = rgba(255, 0, 0, 255);
