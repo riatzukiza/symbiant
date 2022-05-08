13de153e693d5923da47a95ddfa9b7723dbbfdb0
Aaron Beavers <foamy125@gmail.com>
2022-05-08 14:29:41 -0400

    compiled /home/aaronb/devel/err-studios/symbiant/games/engine/sibilant/js/collision.sibilant

--- games/engine/js/collision.js
+++ games/engine/js/collision.js
@@ -77,7 +77,7 @@ game.events.on("collision", ([ c, c_, d ]) => {
       }).call(this) ]);
     } else if (!(((c.entity === home && c_.entity === home) || (c.entity === target && c_.entity === target)))) {
       console.log("ant is colliding with another ant", c, c_);
-      synth.triggerAttackRelease("A3", "64n");
+      synth.triggerAttackRelease("A3", "256n");
       cv.accelerate([ (function() {
         /* eval.sibilant:33:8 */
       
