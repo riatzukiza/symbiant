58a23d5d7001a4c42ecfa0d32bd837b126b10701
Aaron Beavers <foamy125@gmail.com>
2022-05-07 20:05:11 -0400

    compiled /home/aaronb/devel/err-studios/symbiant/games/engine/sibilant/js/forces/signal-field.sibilant

---
+++ games/engine/js/forces/signal-field.js
@@ -0,0 +1,38 @@
+var { 
+  Position
+ } = require("sibilant-game-engine/client/systems/position"),
+    { 
+  Physics
+ } = require("sibilant-game-engine/client/systems/physics"),
+    { 
+  createVectorField,
+  updateParticle
+ } = require("./field"),
+    { 
+  Collision
+ } = require("sibilant-game-engine/client/systems/collision");
+var config = require("../config");
+var SignalField = Physics.Force.define("SignalField", { 
+  field:createVectorField(config.columns, config.rows),
+  layer:createVectorField(config.columns, config.rows),
+  apply( c = this.c,field = this.field,layer = this.layer ){ 
+    
+      var v = c.velocity;
+      var collision = c.system.process.systems.get(Collision, c.entity);
+      return (function() {
+        if (!(collision.colliding)) {
+          updateParticle(v, v.pos, field, layer, game.ticker.ticks, false, false, homePos);
+          var winRate = (v.winCount / ((1 + v.looseCount) || 1));
+          return c.scale = (function() {
+            if (winRate > 1) {
+              return winRate;
+            } else {
+              return 1;
+            }
+          }).call(this);
+        }
+      }).call(this);
+    
+   }
+ });
+exports.SignalField = SignalField;
