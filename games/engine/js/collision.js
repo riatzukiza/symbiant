44865bb67b807cbddac9c1d80a19f3596792c081
Aaron Beavers <foamy125@gmail.com>
2022-05-07 20:59:54 -0400

    compiled /home/aaronb/devel/err-studios/symbiant/games/engine/sibilant/js/collision.sibilant

--- games/engine/js/collision.js
+++ games/engine/js/collision.js
@@ -6,8 +6,13 @@ var {
  } = require("sibilant-game-engine/client/systems/physics"),
     { 
   Velocity
- } = require("sibilant-game-engine/client/systems/velocity");
-game.events.on("collision", ([ c, c_, d ]) => {
+ } = require("sibilant-game-engine/client/systems/velocity"),
+    { 
+  home,
+  homePos,
+  target
+ } = undefined;
+game.events.on("collision", (([ c, c_, d ]) => {
 	
   var cv = game.systems.get(Velocity, c.entity);
   var c_v = game.systems.get(Velocity, c_.entity);
@@ -24,15 +29,15 @@ game.events.on("collision", ([ c, c_, d ]) => {
       c_v.pos.y = hpos.y;
       updateParticle(c_v, c_v.pos, SignalField.field, SignalField.layer, game.ticker.ticks, true, true, homePos);
       return c_v.accelerate([ (function() {
-        /* eval.sibilant:33:8 */
+        /* eval.sibilant:31:8 */
       
-        let rand = ((Math.random() * (config.collisionStatic - 0)) + 0);
-        return (config.collisionStatic - (rand / 2));
+        var rand = ((Math.random() * (config.collisionStatic - 0)) + 0);
+        return (config.collisionStatic - (rand * 2));
       }).call(this), (function() {
-        /* eval.sibilant:33:8 */
+        /* eval.sibilant:31:8 */
       
-        let rand = ((Math.random() * (config.collisionStatic - 0)) + 0);
-        return (config.collisionStatic - (rand / 2));
+        var rand = ((Math.random() * (config.collisionStatic - 0)) + 0);
+        return (config.collisionStatic - (rand * 2));
       }).call(this) ]);
     } else if (c_.entity === target) {
       c.colliding = false;
@@ -42,42 +47,47 @@ game.events.on("collision", ([ c, c_, d ]) => {
       cv.pos.y = hpos.y;
       updateParticle(cv, cv.pos, SignalField.field, SignalField.layer, game.ticker.ticks, true, true, homePos);
       return cv.accelerate([ (function() {
-        /* eval.sibilant:33:8 */
+        /* eval.sibilant:31:8 */
       
-        let rand = ((Math.random() * (config.collisionStatic - 0)) + 0);
-        return (config.collisionStatic - (rand / 2));
+        var rand = ((Math.random() * (config.collisionStatic - 0)) + 0);
+        return (config.collisionStatic - (rand * 2));
       }).call(this), (function() {
-        /* eval.sibilant:33:8 */
+        /* eval.sibilant:31:8 */
       
-        let rand = ((Math.random() * (config.collisionStatic - 0)) + 0);
-        return (config.collisionStatic - (rand / 2));
+        var rand = ((Math.random() * (config.collisionStatic - 0)) + 0);
+        return (config.collisionStatic - (rand * 2));
       }).call(this) ]);
     } else if (!((c.entity === home || c_.entity === home || c.entity === target || c_.entity === target))) {
       c.colliding = false;
       c_.colliding = false;
       cv.accelerate([ (function() {
-        /* eval.sibilant:33:8 */
+        /* eval.sibilant:31:8 */
       
-        let rand = ((Math.random() * (config.collisionStatic - 0)) + 0);
-        return (config.collisionStatic - (rand / 2));
+        var rand = ((Math.random() * (config.collisionStatic - 0)) + 0);
+        return (config.collisionStatic - (rand * 2));
       }).call(this), (function() {
-        /* eval.sibilant:33:8 */
+        /* eval.sibilant:31:8 */
       
-        let rand = ((Math.random() * (config.collisionStatic - 0)) + 0);
-        return (config.collisionStatic - (rand / 2));
+        var rand = ((Math.random() * (config.collisionStatic - 0)) + 0);
+        return (config.collisionStatic - (rand * 2));
       }).call(this) ]);
       return c_v.accelerate([ (function() {
-        /* eval.sibilant:33:8 */
+        /* eval.sibilant:31:8 */
       
-        let rand = ((Math.random() * (config.collisionStatic - 0)) + 0);
-        return (config.collisionStatic - (rand / 2));
+        var rand = ((Math.random() * (config.collisionStatic - 0)) + 0);
+        return (config.collisionStatic - (rand * 2));
       }).call(this), (function() {
-        /* eval.sibilant:33:8 */
+        /* eval.sibilant:31:8 */
       
-        let rand = ((Math.random() * (config.collisionStatic - 0)) + 0);
-        return (config.collisionStatic - (rand / 2));
+        var rand = ((Math.random() * (config.collisionStatic - 0)) + 0);
+        return (config.collisionStatic - (rand * 2));
       }).call(this) ]);
     }
   }).call(this);
 
-});
+})).once("error", ((err) => {
+	
+  console.log("error on", "collision", "of", "game.events", "given", "[ c, c_, d ]()");
+  return console.log(err);
+
+}));
