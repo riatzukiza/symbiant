eed00beca6ab3ef1778cdb456e079a8a8d2eeff8
Aaron Beavers <foamy125@gmail.com>
2022-05-08 14:56:33 -0400

    compiled /home/aaronb/devel/err-studios/symbiant/games/engine/sibilant/js/collision.sibilant

--- games/engine/js/collision.js
+++ games/engine/js/collision.js
@@ -32,7 +32,7 @@ game.events.on("tick", () => {
       isCollision = false;
       return (function() {
         try {
-          return synth.triggerAttackRelease("A3", "4n");
+          return synth.triggerAttackRelease("A3", "32n");
         } catch (e) {
           return console.log("couldn't make a sound");
         }
