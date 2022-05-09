2cb37bf52c4025046940f6d09d2a1e04213c9235
Aaron Beavers <foamy125@gmail.com>
2022-05-08 20:17:02 -0400

    compiled /home/aaronb/devel/err-studios/symbiant/games/engine/sibilant/js/collision.sibilant

--- games/engine/js/collision.js
+++ games/engine/js/collision.js
@@ -44,7 +44,7 @@ const looseLoop=(new Tone.Loop(((time) => {
 	
   return (function() {
     if (isLoose) {
-      looseSynth.triggerAttackRelease("B5", "32n", time);
+      looseSynth.triggerAttackRelease("B2", "32n", time);
       return isLoose = false;
     }
   }).call(this);
