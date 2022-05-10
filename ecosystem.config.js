ebced8592c7c3c90d23f0291252503873ac6b7c4
Aaron Beavers <foamy125@gmail.com>
2022-05-10 19:21:23 -0400

    add pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+const Path = require("path")
+module.exports = {
+  apps : [{
+    script: 'index.sibilant',
+    interpreter:Path.resolve("./node_modules/sibilant/bin/sibilant"),
+    interpreter_args:"-x",
+    ignore_watch:"./games/*/js/**.js",
+    watch: '.'
+
+  },],
+
+  deploy : {
+    production : {
+      user : 'SSH_USERNAME',
+      host : 'SSH_HOSTMACHINE',
+      ref  : 'origin/master',
+      repo : 'GIT_REPOSITORY',
+      path : 'DESTINATION_PATH',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
